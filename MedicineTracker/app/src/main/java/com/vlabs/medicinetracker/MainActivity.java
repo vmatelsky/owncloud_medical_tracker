e42902333e26e1abcaf3f4586c638475d5cd6118
Matelsky, Vladislav <VMatelsky@qulix.com>
2016-03-02 21:04:06 +0300

    implement buttons on main screen

--- MedicineTracker/app/src/main/java/com/vlabs/medicinetracker/MainActivity.java
+++ MedicineTracker/app/src/main/java/com/vlabs/medicinetracker/MainActivity.java
@@ -1,7 +1,12 @@
 package com.vlabs.medicinetracker;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
+
+    @OnClick(R.id.height_weight_button)
+    void onHeightAndWeightClicked(final View view) {
+        Snackbar.make(view, getString(R.string.height_and_weight) + " clicked", Snackbar.LENGTH_LONG).show();
+    }
+
+    @OnClick(R.id.medicine_reminder)
+    void onMedicineReminderClicked(final View view) {
+        Snackbar.make(view, getString(R.string.medicine_reminder) + " clicked", Snackbar.LENGTH_LONG).show();
+    }
+
 }
